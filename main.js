0362dc37fc396259a5ee701e57e5d0a55ea2aa76
José Navarro <navrojd77@gmail.com>
2023-02-16 22:11:57 -0600

    init front end page

---
+++ main.js
@@ -0,0 +1,22 @@
+const { app, BrowserWindow} = require("electron")
+
+
+function createWindow(){
+
+    const mainWindow = new BrowserWindow({
+
+        width:800,
+        height:600
+
+    })
+
+    mainWindow.loadFile('src/views/index.html')
+
+}
+
+
+app.whenReady().then(() => {
+ 
+    createWindow() 
+
+})
