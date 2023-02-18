a90bbd5bf01cb04c4cace3df208766ef995d878b
José Navarro <navrojd77@gmail.com>
2023-02-17 18:41:15 -0600

    add init new window

--- main.js
+++ main.js
@@ -1,22 +1,43 @@
+
+
 const { app, BrowserWindow} = require("electron")
 
 
-function createWindow(){
+function createMainWindow(){
 
-    const mainWindow = new BrowserWindow({
-
-        width:800,
-        height:600
-
-    })
-
+    const { screen } = require('electron')
+    const primaryDisplay = screen.getPrimaryDisplay()
+    const { width, height } = primaryDisplay.workAreaSize
+    
+    mainWindow = new BrowserWindow({ width, height})
+    
     mainWindow.loadFile('src/views/index.html')
+    
 
 }
 
+function createAddColaboradorWindow(){
+
+    const addColaboradorWindow = new BrowserWindow({
+    
+        width: 1000,
+        height: 700,
+        modal: true,
+        show: false,
+        parent: mainWindow
+
+    })
+
+    addColaboradorWindow.loadFile('src/views/addCol.html')
+
+    addColaboradorWindow.once("ready-to-show", () => {
+        addColaboradorWindow.show();
+    });
+
+}
 
 app.whenReady().then(() => {
  
-    createWindow() 
+    createMainWindow() 
 
 })
