2c7e86b7e4ac2545dafd913a4c1dac2dd26b8df8
José Navarro <navrojd77@gmail.com>
2023-02-25 10:44:19 -0600

    personalizar botones y campos del add col

---
+++ src/routes/colaborador.js
@@ -0,0 +1,10 @@
+
+const formAddColaborador = document.getElementById('idFormAddColaborador');
+
+formAddColaborador.addEventListener('submit',(e) => {
+
+    e.preventDefault()
+
+    console.log("Hello Nano")
+
+})
